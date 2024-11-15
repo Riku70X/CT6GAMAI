b80132605905b1f98bb2c86787ef35bb0804cc64
Alex <up2060082@myport.ac.uk>
2024-11-15 16:15:07 +0000

    cleanup of Vehicle.cs

--- CT6GAMAI-Artefact/Assets/Scripts/Actors/Vehicle.cs
+++ CT6GAMAI-Artefact/Assets/Scripts/Actors/Vehicle.cs
@@ -4,32 +4,27 @@ using UnityEngine;
 
 public class Vehicle : MonoBehaviour
 {
-    /////////////////////
-    //Updated Values
-    /////////////////////
-    /// <summary>
-    /// This is applied to the current position every frame
-    /// </summary>
+    //"Updated" Values
+
+    [Tooltip("This is applied to the current position every frame")]
     public Vector3 Velocity;
 
     //Position, Heading and Side can be accessed from the transform component with transform.position, transform.forward and transform.right respectively
 
-    //"Constant" values, they are public so we can adjust them through the editor
+    //"Constant" values, they are serialized so we can adjust them through the editor
 
-    //Represents the weight of an object, will effect its acceleration
-    public float Mass = 1;
+    [Tooltip("Represents the weight of an object, will effect its acceleration")] [SerializeField]
+    private float Mass = 1;
 
-    //The maximum speed this agent can move per second
-    public float MaxSpeed = 1;
+    [Tooltip("The maximum speed this agent can move per second")] [SerializeField]
+    private float MaxSpeed = 1;
 
-    //The thrust this agent can produce
-    public float MaxForce = 1;
+    [Tooltip("The thrust this agent can produce")] [SerializeField]
+    private float MaxForce = 1;
 
-    //We use this to determine how fast the agent can turn, but just ignore it for, we won't be using it
-    public float MaxTurnRate = 1.0f;
+    [Tooltip("We use this to determine how fast the agent can turn, but just ignore it for, we won't be using it")] [SerializeField]
+    private float MaxTurnRate = 1.0f;
 
-
-    // Update is called once per frame
     void Update()
     {
         Vector3 SteeringForce = Vector3.zero;
