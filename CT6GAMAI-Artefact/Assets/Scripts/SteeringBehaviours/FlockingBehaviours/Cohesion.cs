4363df264a677d640cafc592d8075414bdf2ad0a
Alex <up2060082@myport.ac.uk>
2024-12-02 14:08:16 +0000

    Added Cohesion logic

---
+++ CT6GAMAI-Artefact/Assets/Scripts/SteeringBehaviours/FlockingBehaviours/Cohesion.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Returns a force that steers an agent towards the average position of all its neighbours
+/// </summary>
+public class Cohesion : SteeringBehaviourBase
+{
+    public override Vector3 Calculate()
+    {
+        Vector3 steeringForce = Vector3.zero;
+        Vector3 averagePosition = Vector3.zero;
+
+        GameObject[] neighbours = GlobalSteeringFunctions.GetAllNearbyAgents(gameObject, VehicleComponent.GetVisionRadius(), VehicleComponent.GetVisionAngle() * Mathf.Deg2Rad);
+
+        if (neighbours.Length != 0)
+        {
+            foreach (GameObject neighbour in neighbours)
+            {
+                averagePosition += neighbour.transform.position;
+            }
+
+            averagePosition /= neighbours.Length;
+
+            steeringForce = Seek.GetSeekingForceToLocation(VehicleComponent, transform.position, averagePosition);
+        }
+
+        return steeringForce;
+    }
+}
